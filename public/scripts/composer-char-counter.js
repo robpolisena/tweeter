3917a22a67b7d915a762d46e85e1b5a5094110d4
Rob <robpolisena@gmail.com>
2020-07-15 02:10:10 +0000

    add jquery function for character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#tweet-text').on('input', function(event) {
+    let num = 140;
+    let count = $(this).val().length;
+    let keysLeft = num - count;
+    const $parent = $(this).closest('form');
+    const $keyleft = $parent.find('.counter');
+    $keyleft.text(keysLeft);
+    if (keysLeft < 0) {
+      $keyleft.addClass('error');
+    } else {
+      $keyleft.removeClass('error');
+    }
+
+  });
+});
